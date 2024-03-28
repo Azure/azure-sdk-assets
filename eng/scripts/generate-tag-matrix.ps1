862db255e9aef6df7b5be053f5eecb4fd9523b18
Scott Beddall (from Dev Box) <scbedd@microsoft.com>
2024-03-28 16:37:11 -0700

    prepare this whole thing to run

--- eng/scripts/generate-tag-matrix.ps1
+++ eng/scripts/generate-tag-matrix.ps1
@@ -1,36 +1,80 @@
-Set-Strict -Version 4
-
 param(
+    [string] $MatrixFile,
     [string] $RepoWithTags, 
     [string] $Since = $null
 )
 
-[DateTime]$SinceDate = $null
-if ($Since -ne $null) {
-    $SinceDate = [DateTime]::Parse($Date)
-}
-else {
-    $SinceDate = [DateTime]::UtcNow.AddMinutes(-20)
+[DateTime]$SinceDate = [DateTime]::UtcNow.AddMinutes(-20)
+if ($Since -ne $null -or $Since -ne "") {
+    $SinceDate = [DateTime]::Parse($Since)
 }
 
+$success = $true
 $MATRIX_BASE = [PSCustomObject]@{
     "matrix" = @{
         "Agent" = @{
             "ubuntu-20.04" = @{
-                "OSVmImage" = "env:LINUXVMIMAGE"
-                "Pool" = "env:LINUXPOOL"
-                "os" = "linux"
+                "OSVmImage" = "azsdk-pool-mms-ubuntu-2004-1espt"
+                "Pool" = "azsdk-pool-mms-ubuntu-2004-general"
             }
         }
-        "Tag" = @()
+        "Tags" = @()
     }
 }
 
 try {
     Push-Location $RepoWithTags
+    git fetch --tags
 
+    if ($LASTEXITCODE -ne 0) {
+        Write-Error "Unable to fetch tags for $RepoWithTags"
+        $success = $false
+    }
 
+    $allTags = git log --date=iso --tags --simplify-by-decoration --pretty="format:%ai::%d"
+    $tagObjects = @()
+    $results = @()
+  
+    # parse the log output to get the latest tags
+    foreach($tagObject in $allTags)
+    {
+      $parts = $tagObject.Split("::")
+      foreach($part in $parts){ $part = $part.Trim() }
+  
+      $TagRegex = "tag\: (?<tag>[^,)]*)"
+      $tagList = @()
+  
+      Select-String $TagRegex -input $parts[1] -AllMatches `
+        | % { $_.matches } `
+        | % { $tagList += $_.Groups['tag'].Value }
+  
+      $tagObjects += New-Object PSObject -Property @{
+        Date = [DateTime]::Parse($parts[0])
+        TagList = $tagList
+      }
+    }
+    
+    $filteredTagObjects = $tagObjects | Where-Object { $_.TagList.Length -gt 0 -and $_.Date -ge $SinceDate}
+    $filteredTagObjects | Sort-Object -Property Date -Descending
+    
+    foreach($tagObject in $filteredTagObjects)
+    {
+      foreach($tag in $tagObject.TagList)
+      {
+        $results += $tag
+      }
+    }
+
+    $MATRIX_BASE.matrix.Tags = $results | Sort-Object -Unique
 }
 finally {
     Pop-Location
+}
+
+if ($success) {
+    $MATRIX_BASE | ConvertTo-Json -Depth 40 | Out-File $MatrixFile
+}
+else {
+    Write-Error "Failed to generate tag matrix"
+    exit 1
 }
